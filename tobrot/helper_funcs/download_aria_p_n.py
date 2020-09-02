80fb9a4ce309d65de3bbedb34cd2da6b42ac44b3
Hemchand-King <70268346+Hemchand-King@users.noreply.github.com>
2020-09-02 10:55:45 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -439,7 +439,7 @@ async def check_progress_for_dl(aria2, gid, event, previous_message):
                     pass
                 #
                 msg = f"\nDownloading File: `{downloading_dir_name}`"
-                msg += f"\nSpeed: {file.download_speed_string()} 🔽 / {file.upload_speed_string()} 🔼"
+                msg += f"\n<b>○ Speed:</b> <code>{file.download_speed_string()} ⬇️ || {file.upload_speed_string()} ⬆️</code>"
                 msg += f"\nProgress: {file.progress_string()}"
                 msg += f"\nTotal Size: {file.total_length_string()}"
 
