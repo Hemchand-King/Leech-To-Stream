f1f688c51a20c7c38aff9c6d3c09ed19aa370cc9
Hemchand-King <70268346+Hemchand-King@users.noreply.github.com>
2020-09-02 14:36:46 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -450,7 +450,7 @@ async def check_progress_for_dl(aria2, gid, event, previous_message):
 
                 # msg += f"\nStatus: {file.status}"
                 msg += f"\nETA: {file.eta_string()}"
-                msg += f"\nGID: <code>{gid}</code>"
+                 msg += f"\n<b>○ GID:</b> <code>{gid}</code>\n\n<b>© @S4HChannel | @Givenaway</b>"
                 inline_keyboard = []
                 ikeyboard = []
                 ikeyboard.append(InlineKeyboardButton("❌ Cancel ❌", callback_data=(f"cancel {gid}").encode("UTF-8")))
