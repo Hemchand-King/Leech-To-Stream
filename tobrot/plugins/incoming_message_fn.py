c0cb997bc6d340d11ecd4b7ad573e3c98f8e7e1a
Hemchand-King <70268346+Hemchand-King@users.noreply.github.com>
2020-09-02 10:50:21 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -96,7 +96,7 @@ async def incoming_message_f(client, message):
             await i_m_sefg.edit_text(err_message)
     else:
         await i_m_sefg.edit_text(
-            "**FCUK**! wat have you entered. \nPlease read /help \n"
+            "**Hey**! what have you entered. \nPlease read /help \n"
             f"<b>API Error</b>: {cf_name}"
         )
 #
